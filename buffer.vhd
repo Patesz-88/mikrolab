9327bf33ac2af4cd35198fb4aee0b82c53d76a95
Patesz-88 <96594597+Patesz-88@users.noreply.github.com>
2023-03-29 15:39:38 +0200

    Add files via upload

---
+++ buffer.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+---------------------------------------------------------------------------------------------------
+entity buffer_interface is
+	generic(
+		depth: integer range from 0 to 65535;
+		
+	);
+	port (
+		clk:						in	std_logic;
+		reset_n:					in	std_logic;
+		put_char:				in std_logic_vector(15 downto 0);
+		get_char:				out std_logic_vector(15 downto 0);
+		user_logic_intr		out std_logic
+		);
+		
+architecture character_buffer of buffer_interface is
+	signal char_out: 			std_logic_vector(15 downto 0);
+	signal char_in:			std_logic_vector(15 downto 0);
+	signal intr:				std_logic;
+	signal space:  			integer range 0 to 2**depth;
