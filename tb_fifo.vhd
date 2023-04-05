997d1e924401a2b76189c0aada258aba89459070
Patesz-88 <96594597+Patesz-88@users.noreply.github.com>
2023-04-05 17:08:59 +0200

    Add files via upload

--- tb_fifo.vhd
+++ tb_fifo.vhd
@@ -2,10 +2,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 -----------------------------------------------------------------------------------
-entity testbench is
-end entity testbench;
+entity testbench_fifp is
+end entity testbench_fifo;
 -----------------------------------------------------------------------------------
-architecture buffer_testbench of testbench is 
+architecture buffer_testbench of testbench_fifo is 
 
 constant clk_p: 	time 			:= 5ns;
 
@@ -63,26 +63,71 @@ BUF: entity work.edac_protected_fifo(rtl)
 --------------------------------------------------------------------------
 	);
 
-CLOCK: process begin
-wait for clk_p;
-clk 		<= not clk;
+CLOCK: process begin 
+wait for clk_p/2;
+clk 		    <= not clk;
 end process;
 TEST: process begin
 wait for 4*clk_p;
-reset_n 	<= '1';
+reset_n 	 <= '1';
 wait for 2*clk_p;
-data_in 	<= x"0101";
+data_in 	 <= x"0101";
 wait for 2*clk_p;
-push		<= '1';
+push		    <= '1';
 wait for 1*clk_p;
-data_in 	<= x"1010";
+data_in 	 <= x"1010";
 wait for 1*clk_p;
-push 		<= '0';
+push 		   <= '0';
 wait for 1*clk_p;
-push		<= '1';
+push		    <= '1';
 wait for 1*clk_p;
-push 		<= '0';
+push 		   <= '0';
 wait for 4*clk_p;
+pop       <= '1';
+wait for 1*clk_p;
+pop       <= '0';
+wait for 2*clk_p;
+data_in   <= x"1001";
+push      <= '1';
+pop       <= '1';
+wait for 2*clk_p;
+push      <= '0';
+pop       <= '0';
+wait for 4*clk_p;
+data_in 	 <= x"1111";
+push      <= '1';
+wait for 1*clk_p;
+push      <= '0';
+wait for 1*clk_p;
+pop       <= '1';
+wait for 4*clk_p;
+data_in 	 <= x"0110";
+push      <= '1';
+wait for 1*clk_p;
+push      <= '0';
+wait for 1*clk_p;
+data_in 	 <= x"0000";
+push      <= '1';
+wait for 1*clk_p;
+pop       <= '0';
+wait for 1*clk_p;
+push      <= '0';
+wait for 2*clk_p;
+pop       <= '1';
+wait for 1*clk_p;
+pop       <= '0';
+wait for 1*clk_p;
+pop       <= '1';
+wait for 1*clk_p;
+pop       <= '0';
+wait for 1*clk_p;
+pop       <= '1';
+wait for 1*clk_p;
+pop       <= '0';
+wait for 4*clk_p;
+pop       <= '1';
+wait for 1*clk_p;
+pop       <= '0';
 
 
 
